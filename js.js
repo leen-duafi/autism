f15d085bc87d88bc9761a4cb97b9221e25c4e7d8
leen2021 <>
2021-05-05 11:44:40 +0000

    js

---
+++ js.js
@@ -0,0 +1,24 @@
+// hi this is my first page i have to do three dynamic change let us do it 
+
+// change 1 : pop up massage with HELLO MOTHER 
+
+alert("Autism is part of my child, it’s not everything he is. My child is so much more than a diagnosis    S.L. Coelho, author");
+
+// nice! change 2 : 
+
+var doniate=prompt('would like to donate?');
+
+if (doniate=="yes")
+{ window.location = "https://www.autismspeaks.org/?form=donate"}
+
+else(doniate=="no")
+{ document.write('<img src="https://pbs.twimg.com/media/Eqf1WzpXAAEhRa1.jpg">')}
+
+// change 3
+
+var autism=prompt("relationship to autism")
+
+console.log(autism)
+
+
+
