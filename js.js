ed2c2f04f770b609e916b6cc12615e999e1c4bb0
leen2021 <>
2021-05-06 11:49:28 +0000

    js

--- js.js
+++ js.js
@@ -14,19 +14,27 @@ console.log(autism)
 
 
 
-var doniate=prompt('would like to donate?');
+var donate =prompt('would like to donate?');
 
-if (doniate=="yes")
+while ( donate !="yes" && donate!= "no"){donate = prompt("please answer the question first") }
+
+if (donate=="yes")
 
 {alert (" we will lead you to page to donate ")
    window.location = "https://www.autismspeaks.org/?form=donate"}
 
-
-
 else
 { document.write('<img src="https://pbs.twimg.com/media/Eqf1WzpXAAEhRa1.jpg">')}
 
-// change 3
+var usfull =prompt("how much do you find our page usefull from 1 to 3 (3 good and 1 bad )");
+
+
+for (var i=0;i<usfull;i++){  if (i==3){i=usfull+1}
+else {
+  document.write('<img src="https://www.clipartmax.com/png/middle/437-4371898_tumblr-heart-transparent-love-transparent-background-heart-png.png" width="50px">')}
+ 
+}
+
 
 
 
